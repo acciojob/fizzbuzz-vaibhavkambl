48736104fbcae1c50644ac76d1c43fb24cc0890f
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-16 16:21:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let output = "";
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    output += "FizzBuzz";
+  } else if (i % 3 === 0) {
+    output += "Fizz";
+  } else if (i % 5 === 0) {
+    output += "Buzz";
+  } else {
+    output += i;
+  }
+
+  // Add a newline character after each number/string
+  output += "\n";
+}
+
+// Display the result using alert()
+alert(output);
